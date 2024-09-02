95c31799809781a1d4210af9ed7ac0916165008a
Teuku Fandi Ahmad <tfandiahmad32@gmail.com>
2024-09-02 23:38:10 +0700

    gatau dah

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar></Navbar>
+      </section>
+      <section id="Services">Parallax</section>
+      <section>Services</section>
+      <section id="Portfolio">Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* sidebar */}
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          TeukuFandi
+        </motion.span>
+        <div className="social">
+          <a href="https://facebook.com/teukufaandii">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="https://instagram.com/teukufaandii">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="https://youtube.com/@andyy.0903">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="https://github.com/teukufaandii">
+            <img src="/github.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
